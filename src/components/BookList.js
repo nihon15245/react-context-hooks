2217fd5598364632fd2b25e5b54e9bc2c860f1f5
nihon15245 <78631635+nihon15245@users.noreply.github.com>
2025-11-28 20:03:39 +0530

    modified:   src/App.js 	modified:   src/components/BookList.js 	modified:   src/components/Navbar.js 	new file:   src/components/ThemeToggle.js 	new file:   src/contexts/AuthContext.js 	modified:   src/contexts/ThemeContext.js 	modified:   src/index.css 	modified:   src/index.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
             <div className="book-list">
                 <ul className='list-group'>
-                    <li className='list-group-item'>The Great Gatsby</li>
-                    <li className='list-group-item'>To Kill a Mockingbird</li>
-                    <li className='list-group-item'>1984</li>
+                    <li style={{ color: theme.syntax, background: theme.ui }} className='list-group-item'>
+                        <h6>The Great Gatsby</h6>
+                        <p>By example Auther</p>
+                    </li>
+                    <li style={{ color: theme.syntax, background: theme.ui }} className='list-group-item'>
+                        <h6>To Kill a Mockingbird</h6>
+                        <p>By example Auther</p>
+                    </li>
+                    <li style={{ color: theme.syntax, background: theme.ui }} className='list-group-item'>
+                        <h6>1984</h6>
+                        <p>By example Auther</p>
+                    </li>
                 </ul>
             </div>
         );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Reading List</a>
-                    <div className="d-flex">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <AuthContext.Consumer>
+                {(authContext) => {
+                    return (
+                        <ThemeContext.Consumer>
+                            {(themeContext) => {
+                                const { isLightTheme, light, dark } = themeContext;
+                                const theme = isLightTheme ? light : dark;
+                                const { isAuthenticated, toggleAuth } = authContext;
+                                const authStatus = isAuthenticated ? 'Logged In' : 'Logged Out';
+                                return (
+                                    <nav className="navbar navbar-expand" style={{ background: theme.ui, color: theme.syntax }}>
+                                        <div className="container-fluid">
+                                            <a className="navbar-brand" href="#" style={{ color: theme.syntax }}>Reading List</a>
+                                            <div className="d-flex">
+                                                <ul className="navbar-nav">
+                                                    <li className="nav-item">
+                                                        <a className="nav-link active" aria-current="page" href="#" style={{ color: theme.syntax }}>Home</a>
+                                                    </li>
+                                                    <li className="nav-item">
+                                                        <a className="nav-link" href="#" style={{ color: theme.syntax }}>Features</a>
+                                                    </li>
+                                                    <li className="nav-item">
+                                                        <a className="nav-link" href="#" style={{ color: theme.syntax }}>Pricing</a>
+                                                    </li>
+                                                    <li className="nav-item">
+                                                        <a className="nav-link" href="#" style={{ color: theme.syntax }} onClick={toggleAuth}>{authStatus}</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                );
+                            }}
+                        </ThemeContext.Consumer>
+                    )
+                }}
+            </AuthContext.Consumer>
         );
     }
 }
